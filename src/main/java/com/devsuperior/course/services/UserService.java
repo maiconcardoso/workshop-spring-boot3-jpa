81e9746d4ab87a4b6ad463a2ae760a1848a51673
Maicon Cardoso <maiconscardoso@hotmail.com>
2024-08-26 07:09:47 -0300

    User Update

--- src/main/java/com/devsuperior/course/services/UserService.java
+++ src/main/java/com/devsuperior/course/services/UserService.java
@@ -9,6 +9,8 @@ import com.devsuperior.course.repositories.UserRepository;
 import java.util.List;
 import java.util.Optional;
 
+import javax.swing.text.html.parser.Entity;
+
 @Service
 public class UserService {
 
@@ -30,4 +32,18 @@ public class UserService {
     public void delete(Long id) {
         repository.deleteById(id);
     }
+
+    public User update(Long id, User userForUpdated) {
+        User entity = repository.getReferenceById(id);
+        updateData(entity, userForUpdated);
+        return repository.save(entity);
+    }
+
+    private void updateData(User entity, User userForUpdated) {
+        entity.setName(userForUpdated.getName());
+        entity.setEmail(userForUpdated.getEmail());
+        entity.setPhone(userForUpdated.getPhone());
+    }
+
+    
 }
