405109f28546c358560c229de6120f5276764401
Maicon Cardoso <maiconscardoso@hotmail.com>
2024-08-26 06:59:19 -0300

    User Delete

--- src/main/java/com/devsuperior/course/resources/UserResource.java
+++ src/main/java/com/devsuperior/course/resources/UserResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -39,6 +40,12 @@ public class UserResource {
     public ResponseEntity<User> insert(@RequestBody User user) {
         User createdUser = service.insert(user);
         return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
     
 
